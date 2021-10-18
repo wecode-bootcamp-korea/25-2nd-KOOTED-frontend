e0d266df63a82654fb3620e9369db95dd864b65e
Seungkyun <seungkyun.h@gmail.com>
2021-10-18 15:29:39 +0900

    Add: 초기세팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import CompanyList from './pages/CompanyList/CompanyList';
+import CompanyDetail from './pages/CompanyDetail/CompanyDetail';
+import Mypage from './pages/Mypage/Mypage';
+import Resume from './pages/Resume/Resume';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/wd-list" component={CompanyList} />
+          <Route exact path="/wd-detail" component={CompanyDetail} />
+          <Route exact path="/wd-mypage" component={Mypage} />
+          <Route exact path="/wd-resume" component={Resume} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default Routes;
---
+++ src/pages/Mypage/Mypage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Mypages extends React.Component {
+  render() {
+    return null;
+  }
+}
