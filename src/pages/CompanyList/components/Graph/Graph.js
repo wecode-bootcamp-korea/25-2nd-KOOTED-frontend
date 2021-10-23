1773ecf066662c1522544c2552275bfa7915ac6e
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-10-23 12:25:56 +0900

    Add : 그래프 구조 큰 틀 구축
    Modify : scss 큰 틀 수정 - 가운데 정렬
    
    Add : 그래프 레이아웃 구현 완료 / 반복되는 정적인 요소 상수 데이터로 mapping
    
    Add : 백엔드 데이터 받아와서 각 그래프마다 수치 표현 완료 / 각 그래프의 수치에 대한 평균값 완료
    
    Modify : 평균 연봉 콤마 찍는 정규식 추가
    
    Add : 막대그래프 마우스 호버시 연봉 정보가 나오는 말풍선 추가
    
    Add : 그래프 올라가는 애니메이션 css 추가
    
    Modify: 사이더 에러 수정 -> valueGraph 파일에서 useEffect [] 에서 [salarydata]로 변경
    
    Modify : 정적 데이터 배열 수정
    
    Fix : 리팩토링 (상수 데이터화, 연산 메서드 정리)
    
    Fix : 리팩토링 후 라우트에 헤더 추가

---
+++ src/pages/CompanyList/components/Graph/Graph.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import ValueNum from './ValueNum';
+import ValueLine from './ValueLine';
+import ValueGraph from './ValueGraph';
+import './Graph.scss';
+
+export default function Graph() {
+  const [salaryData, setSalaryData] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/companyList/Graph.json')
+      .then(res => res.json())
+      .then(data => setSalaryData(data.salary_list));
+
+    valueToAVG(salaryData);
+  }, [salaryData]);
+
+  const valueToAVG = salaryData => {
+    let floatToIntArr = salaryData.map(data =>
+      Number(data.total_salary.split('.')[0])
+    );
+    let sum = floatToIntArr.reduce((prev, curr) => prev + curr, 0);
+    let avgSum = Math.floor(sum / 110000).toLocaleString();
+
+    return avgSum;
+  };
+
+  return (
+    <article className="Graph">
+      <div className="graphWrap">
+        <div className="valueNumWrap">
+          {Y_LINE.map((yValue, idx) => (
+            <ValueNum key={idx} yearMoney={yValue} />
+          ))}
+        </div>
+        <div className="valueLineWrap">
+          {GRAPH_LINE.map((numberLine, idx) => (
+            <ValueLine key={idx} />
+          ))}
+        </div>
+        <div className="valueGraphWrap">
+          {salaryData.map(data => (
+            <ValueGraph
+              key={data.working_year}
+              careerYears={data.working_year}
+              totalSalary={data.total_salary}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="textWrap">
+        <button className="job_group">개발</button>
+        <button className="job">프론트엔드 개발자</button>
+        <div className="avgWrap">
+          <p className="avgText">프론트엔드 개발자 평균 연봉</p>
+          <p className="avgValueWrap">
+            <span className="avgValue">{valueToAVG(salaryData)}</span> 만원
+          </p>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+const Y_LINE = [
+  '7,000',
+  '6,000',
+  '5,000',
+  '4,000',
+  '3,000',
+  '2,000',
+  '1,000',
+  '0',
+];
+
+const GRAPH_LINE = new Array(7).fill('ValueLine');
---
+++ src/pages/CompanyList/components/Graph/ValueLine.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './ValueLine.scss';
+
+export default function ValueLine() {
+  return <div className="ValueLine" />;
+}
