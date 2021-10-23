ecdb3f332001a256579867cacf4adc1c7335b6af
jeong-minji <alswl4152@naver.com>
2021-10-23 15:10:05 +0900

    Add: 사용자 정보 및 북마크 리스트 API 통신 구현 완료

--- src/pages/Mypage/SideContent/UserInfo/UserInfo.js
+++ src/pages/Mypage/SideContent/UserInfo/UserInfo.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './UserInfo.scss';
 
-const UserInfo = ({ image, name, email, phone }) => {
+const UserInfo = ({ name, email, mobile_number }) => {
   return (
     <div className="UserInfo">
-      <img className="profile" src={image} alt="userProfile" />
+      <img
+        className="profile"
+        src="/images/MyPage/profile.png"
+        alt="userProfile"
+      />
       <h3 className="name">{name}</h3>
       <h5 className="email">{email}</h5>
-      <h5 className="contact">{phone}</h5>
+      <h5 className="contact">{mobile_number}</h5>
       <button className="btnAddInterest">
         <i className="fas fa-plus-square" />
         관심사 선택하기
